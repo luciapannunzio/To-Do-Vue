c3f522694948f697febe67258d386b7d7c60f213
luciapannunzio <luciapannunzio@gmail.com>
2023-06-13 15:38:48 +0200

    errors fixed

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { ref, onMounted, computed, watch } from "vue";
 const todos = ref([]);
 const name = ref("");
 
-const input_content = ref();
+const input_content = ref("");
 const input_category = ref(null);
 
 const todos_asc = computed(() =>
